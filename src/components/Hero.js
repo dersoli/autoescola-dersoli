06bba95abf610f17b15bf7a2c4681f85a7997e2c
Anderson Xavier <dersoli@gmail.com>
2023-04-06 10:03:56 -0300

    Hero component added

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = ({title, children}) => (
+    <div>
+        <div>
+            <h1>{title}</h1>
+            {children}
+        </div>
+    </div>
+);
+
+export default Hero;
